5187759c05a8c424f209e307bbe19b11ab2e84e6
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-06-06 16:12:36 +0100

    hero fixed

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -5,18 +5,17 @@ import styles from './Hero.module.scss';
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      
       <div className="container">
-      <div className={styles.logo}>
-        <Icon icon="logo" />
-      </div> 
+        <div className={styles.logo}>
+          <Icon icon="logo" />
+        </div>
         <div className={styles.heroColumn}>
           <div className={styles.heroDesc}>
-            <h3>One wallet for all your payment needs.</h3>
+            <h3>One app for all your payment needs.</h3>
             <h5 className="text-light py-40">
-              The most seamless way to send & recieve money, pay electricity
-              bills, cable subscription, buy airtime, buy data, pay for
-              insurance.
+              The most seamless way to pay your electricity bills, cable
+              subscription, buy airtime, buy data, pay for insurance and
+              transfer money.
             </h5>
             <div className={styles.btnWrap}>
               <div className={styles.appButton}>
