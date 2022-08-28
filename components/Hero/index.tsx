f2a7e03ad387bb9360272a3bb1bf6b285b93370e
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-08-28 18:49:27 +0100

    Added app links

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import Icon from '../Icon';
 import styles from './Hero.module.scss';
@@ -14,14 +15,30 @@ const Hero = () => {
                 #Payment<span>MadeEasy</span>
               </h3>
               <h5 className="py-40">
-                Spice up your life with discounted bill payments, affordable insurance packages, swift wallet transactions, a secure virtual dollar card, cash rewards, and more. 
+                Spice up your life with discounted bill payments, affordable
+                insurance packages, swift wallet transactions, a secure virtual
+                dollar card, cash rewards, and more.
               </h5>
               <div className={styles.btnWrap}>
                 <div className={styles.appButton}>
-                  <Icon icon="appStore" />
+                  <Link
+                    href="https://apps.apple.com/ng/app/pawa-payments-cashback/id1631853102"
+                    target="_blank"
+                  >
+                    <a>
+                      <Icon icon="appStore" />
+                    </a>
+                  </Link>
                 </div>
                 <div className={styles.appButton}>
-                  <Icon icon="playStore" />
+                  <Link
+                    href="https://play.google.com/store/apps/details?id=ng.pawa.app"
+                    target="_blank"
+                  >
+                    <a>
+                      <Icon icon="playStore" />
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
